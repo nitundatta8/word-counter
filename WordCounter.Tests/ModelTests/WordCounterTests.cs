c94b35ac9e97cf1f44885d97a3d0a3dea3d75780
nitunwa <nitun.2@gmail.com>
2020-05-15 09:01:11 -0700

    add initial counter.soluton project

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Tests{
+  [TestClass]
+  public class WordCounterTests{
+  [TestMethod]
+  public void Count(){
+    
+  }
+  }
+}
---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,5 @@
+namespace WordCounter.Models{
+  public class Counter{
+    
+  }
+}
