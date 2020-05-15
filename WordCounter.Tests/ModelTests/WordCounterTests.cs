849f6c4029f419aef9ebafa9bc08ea4fa336363a
nitunwa <nitun.2@gmail.com>
2020-05-15 11:29:24 -0700

    validate input with C# build in method but didn't get expexted result

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -5,8 +5,21 @@ namespace WordCounter.Tests{
   [TestClass]
   public class WordCounterTests{
   [TestMethod]
-  public void Count(){
-    
-  }
+    public void CounterAppContructor_CreateInstanceOfCounterApp_CounterApp(){
+      string word = "cat";
+      string sentence = "I have a cat in the cathedral";
+      CounterApp counterApp = new CounterApp(word,sentence);
+      Assert.AreEqual(typeof(CounterApp),counterApp.GetType()); 
+    }
+    [TestMethod]
+    public void ValidateStrings_CheackInput_True(){
+      string word = "cat66";
+      string sentence = "I have a at in the cathedral";
+      CounterApp counterApp = new CounterApp(word,sentence);
+      bool result = counterApp.ValidateStrings(counterApp);
+      Assert.AreEqual(false,result); 
+    }
+
+
   }
 }
