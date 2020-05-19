2bfd5e9adbafac888a13490e8297704494196c29
nitunwa <nitun.2@gmail.com>
2020-05-18 17:31:49 -0700

    add changes README file and indentation.

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -1,65 +1,83 @@
 using System;
-namespace WordCounter.Models{
-  public class CounterApp{
+namespace WordCounter.Models
+{
+  public class CounterApp
+  {
     private string _word;
     private string _sentence;
-    public CounterApp(string word ,string sentence){
+    public CounterApp(string word, string sentence)
+    {
       _word = word;
       _sentence = sentence;
     }
-    private bool ValidateLetter(string wordInput){
+    private bool ValidateLetter(string wordInput)
+    {
       string word = wordInput.Replace(" ", string.Empty);
-      char [] charArray = word.ToLower().ToCharArray();
-      foreach(char ch in charArray){
-        if(!Char.IsLetter(ch)){
+      char[] charArray = word.ToLower().ToCharArray();
+      foreach (char ch in charArray)
+      {
+        if (!Char.IsLetter(ch))
+        {
           return false;
         }
       }
-        return true;
+      return true;
     }
-    
-    public bool ValidateStrings(CounterApp counterApp){
+
+    public bool ValidateStrings(CounterApp counterApp)
+    {
       string wordInput = counterApp._word;
       string sentenceInput = counterApp._sentence;
       bool wordResult = ValidateLetter(wordInput);
       bool sentenceResult = ValidateLetter(sentenceInput);
-      
-      if(wordResult && sentenceResult){
+
+      if (wordResult && sentenceResult)
+      {
         return true;
       }
       return false;
     }
-    public bool IsSrtingNullOrEmpty(CounterApp counterApp){
+    public bool IsSrtingNullOrEmpty(CounterApp counterApp)
+    {
       string wordInput = counterApp._word;
       string sentenceInput = counterApp._sentence;
       bool word = string.IsNullOrEmpty(wordInput);
       bool sentence = string.IsNullOrEmpty(sentenceInput);
-      if(!word && !sentence){
+      if (!word && !sentence)
+      {
         return true;
       }
       return false;
     }
-    public bool ContainWord(CounterApp counterApp){
+    public bool ContainWord(CounterApp counterApp)
+    {
       string wordInput = counterApp._word.ToLower();
       string sentenceInput = counterApp._sentence.ToLower();
-      string [] sentence = sentenceInput.Split(" ");
-      for(int i=0; i< sentence.Length;i++){
-        if(sentence[i].Contains(wordInput)){
-          if(sentence[i].Length == wordInput.Length){
+      string[] sentence = sentenceInput.Split(" ");
+      for (int i = 0; i < sentence.Length; i++)
+      {
+        if (sentence[i].Contains(wordInput))
+        {
+          if (sentence[i].Length == wordInput.Length)
+          {
             return true;
           }
         }
       }
       return false;
     }
-    public int CountWords(CounterApp counterApp){
-      int count = 0 ;
+    public int CountWords(CounterApp counterApp)
+    {
+      int count = 0;
       string wordInput = counterApp._word.ToLower();
       string sentenceInput = counterApp._sentence.ToLower();
-      string [] sentence = sentenceInput.Split(" ");
-      for(int i=0; i< sentence.Length;i++){
-        if(sentence[i].Contains(wordInput)){
-          if(sentence[i].Length == wordInput.Length){
+      string[] sentence = sentenceInput.Split(" ");
+      for (int i = 0; i < sentence.Length; i++)
+      {
+        if (sentence[i].Contains(wordInput))
+        {
+          if (sentence[i].Length == wordInput.Length)
+          {
             count++;
           }
         }
--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,33 +1,43 @@
 using System;
 using WordCounter.Models;
 
-namespace WordCounter{
-  public class Program{
-    public static void Start(){
+namespace WordCounter
+{
+  public class Program
+  {
+    public static void Start()
+    {
       Console.WriteLine("Let's check how frequently a word appears in the sentence.");
       Console.WriteLine("Enter a word......");
       string word = Console.ReadLine();
       string wordInput = word.ToUpper();
       Console.WriteLine("Enter a sentence......");
       string sentence = Console.ReadLine();
-      CounterApp counterApp = new CounterApp(word,sentence);
+      CounterApp counterApp = new CounterApp(word, sentence);
       int count = counterApp.CountWords(counterApp);
-      if(count>0){
+      if (count > 0)
+      {
         Console.WriteLine($"{wordInput} appears {count} times in your sentence.");
-      }else{
+      }
+      else
+      {
         Console.WriteLine("No mathching word is found.");
       }
       Console.WriteLine("Would you like to check one more time.[Y/N]");
-      string userInput =Console.ReadLine();
+      string userInput = Console.ReadLine();
       string answer = userInput.ToLower();
-      if(answer == "y"){
+      if (answer == "y")
+      {
         Start();
-      }else{
+      }
+      else
+      {
         Console.WriteLine("Good Bye");
       }
 
     }
-    public static void Main(){
+    public static void Main()
+    {
       Start();
     }
   }
